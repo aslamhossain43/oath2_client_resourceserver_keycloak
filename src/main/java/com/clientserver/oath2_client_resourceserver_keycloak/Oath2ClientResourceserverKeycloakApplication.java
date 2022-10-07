6ea448f77330c5e6c4e6ca32dc237a612fd93e70
aslam-enkaizen <aslam.hossain@enkaizen.com>
2022-10-07 21:47:58 +0600

    initial

---
+++ src/main/java/com/clientserver/oath2_client_resourceserver_keycloak/Oath2ClientResourceserverKeycloakApplication.java
@@ -0,0 +1,13 @@
+package com.clientserver.oath2_client_resourceserver_keycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oath2ClientResourceserverKeycloakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oath2ClientResourceserverKeycloakApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/clientserver/oath2_client_resourceserver_keycloak/Oath2ClientResourceserverKeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clientserver.oath2_client_resourceserver_keycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oath2ClientResourceserverKeycloakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
